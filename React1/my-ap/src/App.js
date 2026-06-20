28d8280f2239ab5eb0b7deaa51077b1408787de9
GHANNATH KUMAR <ghannathk@gmail.com>
2026-06-20 05:22:58 +0000

    Pending changes exported from your codespace

---
+++ React1/my-ap/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar  from './Navbar';
+function App() {
+  let name = "ghannath";
+  return (
+    <>
+    <Navbar title="DesiQna" link="Links"/>
+    </>
+  );
+}
+
+
+export default App;
+
+// functional based : react hooks..
+// props : properties means data passing inside function ..
